ec82f925e0cd7c22040d485e0a1ee6141b7ffb2f
MarcosStanquini <marcossnarquini@gmail.com>
2025-07-12 12:36:28 -0300

    feat: add user post route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-import fastify from "fastify"
+import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './env/lib/prisma'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(appRoutes)
+
