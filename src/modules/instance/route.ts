4f81717dde3cdcd22c7f12740e676f2ea6722fc7
MarcosStanquini <marcossnarquini@gmail.com>
2025-08-08 22:38:40 -0300

    feat: just clean some routes

--- src/modules/instance/route.ts
+++ src/modules/instance/route.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
-import { createInstance } from './controllers/create'
-import { connectInstance } from './controllers/connect'
+
 import { whatsappConnect } from './controllers/handle-whatsapp-connection'
 import { desconnectInstance } from './controllers/desconnect'
 import { verifyConnected } from '@/http/middlewares/verify-connected'
@@ -11,81 +10,13 @@ import { sendMessage } from './controllers/send-message'
 export async function InstanceRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post(
-    '/instance/create',
-    {
-      schema: {
-        description: 'Criar nova instância WhatsApp',
-        tags: ['Instance'],
-        security: [{ Bearer: [] }],
-        response: {
-          201: {
-            type: 'object',
-            properties: {
-              instanceName: { type: 'string' },
-              qrCode: { type: 'string' },
-            },
-          },
-          500: {
-            type: 'object',
-            properties: {
-              message: { type: 'string' },
-            },
-          },
-        },
-      },
-    },
-    createInstance,
-  )
-
   app.get(
-    '/instance/connect/:instanceName',
-    {
-      schema: {
-        description: 'Conectar à instância WhatsApp',
-        tags: ['Instance'],
-        security: [{ Bearer: [] }],
-        params: {
-          type: 'object',
-          required: ['instanceName'],
-          properties: {
-            instanceName: { type: 'string' },
-          },
-        },
-        response: {
-          200: {
-            type: 'object',
-            properties: {
-              status: { type: 'string' },
-              qrCode: { type: 'string' },
-            },
-          },
-          500: {
-            type: 'object',
-            properties: {
-              message: { type: 'string' },
-            },
-          },
-        },
-      },
-    },
-    connectInstance,
-  )
-
-  app.post(
     '/instance/connect',
     {
       schema: {
         description: 'Processar conexão WhatsApp',
         tags: ['Instance'],
         security: [{ Bearer: [] }],
-        body: {
-          type: 'object',
-          required: ['instanceName'],
-          properties: {
-            instanceName: { type: 'string' },
-          },
-        },
         response: {
           200: {
             type: 'object',
